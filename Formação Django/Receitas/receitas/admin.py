532c640a0794dea02b1cb163e0e56919105751a4
Eduardo Marioti <edumarioti@gmail.com>
2022-03-31 22:05:16 -0300

    Ajustes no Admin Django

--- Formação Django/Receitas/receitas/admin.py
+++ Formação Django/Receitas/receitas/admin.py
@@ -1,4 +1,18 @@
+from re import T
+from tkinter import N
 from django.contrib import admin
 from .models import Receita
 
-admin.site.register(Receita)
+
+class ListandoReceitas(admin.ModelAdmin):
+
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo', 'publicada')
+    list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita',)
+    list_filter = ('categoria','publicada')
+    list_editable =  ('publicada',)
+    list_per_page = 20
+
+
+admin.site.register(Receita, ListandoReceitas)
+
